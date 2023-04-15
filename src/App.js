d8d374ca28ca4f180f6bd644bf12ad51fcee54c7
20021341 <80798780+20021341@users.noreply.github.com>
2023-04-15 15:03:53 +0700

    cộm

--- src/App.js
+++ src/App.js
@@ -5,19 +5,39 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import axios from 'axios';
 
 function App() {
+
+
+  const [input, setInput] = useState('')
   const [search, setSearch] = useState(false)
   const [filter, setFilter] = useState('')
   const [movieList, setMovieList] = useState([])
 
+  const MOVIENAME = 'get_movies_by_movie_name'
+  const MOVIEGENRE = 'get_movies_by_genre'
+  const MOVIEYEARELEASE = 'get_movies_by_year_release'
+  const MOVIECREW = 'get_movies_feat_name'
+
   const handleInputChange = (event) => {
-    // switch(filter) {
-    //   case 'Movie\'s name':
-    //     axios()
-    // }
+    setInput(event.target.value)
     if (event.target.value !== '') {
       setSearch(true)
     } else {
       setSearch(false)
+    }
+  }
+
+  const handleInputEnter = (event) => {
+    if (event.key === 'Enter') {
+      console.log(event.key)
+      axios(`http://localhost:3001/api/${MOVIENAME}?movie_name=${input}`)
+          .then(res => {
+            console.log(res.data)
+          })  
+      // switch(filter) {
+      //   case MOVIENAME:
+          
+      //     })
+      // }
     }
   }
 
@@ -37,6 +57,7 @@ function App() {
             type='text'
             placeholder='Enter...'
             onChange={handleInputChange}
+            onKeyDown={handleInputEnter}
           />
           <span className='App-searchicon'>
             <FontAwesomeIcon icon={faMagnifyingGlass}/>
@@ -53,14 +74,14 @@ function App() {
           </div>
         </div>
         <select id='filter' onChange={handleFilterChange}>
-          <option value='get_movies_by_movie_name'>Movie's name</option>
-          <option value='get_movies_by_genre'>Genre</option>
-          <option value='get_movies_by_year_release'>Released year</option>
-          <option value='get_movies_feat_name'>Actor</option>
+          <option value={MOVIENAME}>Movie's name</option>
+          <option value={MOVIEGENRE}>Genre</option>
+          <option value={MOVIEYEARELEASE}>Released year</option>
+          <option value={MOVIECREW}>Actor</option>
         </select>
       </header>
       <body className='App-body'>
-        <div className={window.location == '/' && 'hide'}>Please enter your desired movie choice</div>
+        <div className={window.location === '/' && 'hide'}>Please enter your desired movie choice</div>
       </body>
     </div>
   );
